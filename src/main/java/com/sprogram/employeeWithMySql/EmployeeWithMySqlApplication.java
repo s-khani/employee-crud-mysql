156d569a39ff6475e74d614b1766d5190ec6b0d5
samaneh <s.khanimirzaee@gmail.com>
2022-10-23 23:13:57 +0200

    [init]- project skeleton

---
+++ src/main/java/com/sprogram/employeeWithMySql/EmployeeWithMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.sprogram.employeeWithMySql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWithMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeWithMySqlApplication.class, args);
+	}
+
+}
